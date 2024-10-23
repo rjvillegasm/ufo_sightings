c900b32d6b62aeee3b3182014335d1d3676a2864
roberto <rvillegas@outlook.com>
2024-10-23 19:50:07 -0300

    conexción a postgres

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+import psycopg2
+
+def main():
+    conn = psycopg2.connect('postgres://avnadmin:************************@baal-postgre-myspark.f.aivencloud.com:25000/defaultdb?sslmode=require')
+
+    query_sql = 'SELECT VERSION()'
+
+    cur = conn.cursor()
+    cur.execute(query_sql)
+
+    version = cur.fetchone()[0]
+    print(version)
+
+
+if __name__ == "__main__":
+    main()
+    
